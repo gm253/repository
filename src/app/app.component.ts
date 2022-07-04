74caeb103a9e2253254f6685b2bb42f0d7edbfb2
gm253 <gmtech253@gmail.com>
2022-07-04 11:18:44 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit , importProvidersFrom } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {Observable, observable} from 'rxjs'
+import {map,startWith} from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material';
+  selectedvalue = ''
+  options: string[] = ['angular','react','node'];
+  object = [
+    {
+      name:"angular14"
+
+    },
+  {
+    name:"node15"
+  }]
+  _filtervalue: any;
+  displayfn(subject :any)
+  {
+    return subject ? subject.name : undefined;
+  }
+  mycontrol = new FormControl();
+  filteredoptions!: Observable<string[]>;
+
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  this.filteredoptions = this.mycontrol.valueChanges.pipe(
+    startWith(''),
+    map(value => this._filter(value))
+  );
+}
+private _filter(value: string):string[]{
+  const filtervalue = value.toLowerCase();
+  return this.options.filter(options => options.toLowerCase().includes(filtervalue));
+}
+  obj(obj: any, arg1: any): string[] {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatStepperModule } from '@angular/material/stepper';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatButtonModule} from '@angular/material/button'
+import {MatInputModule} from '@angular/material/input'
+import {MatSelectModule} from '@angular/material/select'
+import {MatAutocompleteModule} from '@angular/material/autocomplete'
+import {ReactiveFormsModule} from  '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatStepperModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
